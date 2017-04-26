6c2d9535883fccb173aa71ec1fbc823fc609edcc
IgnatenkoPavlo <ignatenko.pavlo@yandex.ru>
2017-04-27 01:04:29 +0300

    Fird commit, dummy project with dummy console output

--- LearnOfJava/src/com/company/Pavel/Main.java
+++ LearnOfJava/src/com/company/Pavel/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("first output, from github");
+        System.out.println("first output, from github 111");
     }
 }
